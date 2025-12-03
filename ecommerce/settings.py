c505af072a4342ed20fe6d4a5454d68c3a747d1f
Mohamed Abed <momo55265526@gmail.com>
2025-12-03 06:52:00 +0200

    fdg6545

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p-e-y!))k8q_@$00pirm(@)iykoxtsyokmk06=8((4w+(vtmcj'
+# SECRET_KEY = 'django-insecure-p-e-y!))k8q_@$00pirm(@)iykoxtsyokmk06=8((4w+(vtmcj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
