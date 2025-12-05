40c026630d6e2bd54d86e256c230492558883092
Mohamed Abed <momo55265526@gmail.com>
2025-12-05 20:33:52 +0200

    44rf7

--- api/settings.py
+++ api/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=pclseu)=7*f#d0#zs-h^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# ALLOWED_HOSTS = ['127.0.0.1,elshapar-j3tx-kb7d77hwc-momo551s-projects.vercel.app']
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1,elshapar-j3tx-ilnp0nd6v-momo551s-projects.vercel.app']
 
 
 # Application definition
