c46c6b201d4aafa8389434a5710957ff4af844bf
Mohamed Abed <momo55265526@gmail.com>
2025-12-09 07:26:43 +0200

    asldfda;l;ads

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,17 +1,14 @@
-"""
-WSGI config for api project.
-
-It exposes the WSGI callable as a module-level variable named ``app``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# مسار مشروعك على PythonAnywhere
+project_home = '/home/mohamedelzafar/ecommerce'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
+# ضبط settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
+# استدعاء Django application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-handler = application  # For compatibility with some WSGI servers
