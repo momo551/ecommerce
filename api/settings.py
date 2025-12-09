60bf0cf9c88592ee7eaa73529420c7c59e647fb5
Mohamed Abed <momo55265526@gmail.com>
2025-12-09 06:24:33 +0200

    vds

--- api/settings.py
+++ api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',defasult=False, cast=bool)
+DEBUG = config('DEBUG',default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
