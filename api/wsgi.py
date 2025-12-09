aee20caa6b1502ceaa369207c26ef25c27f25b0a
Mohamed Abed <momo55265526@gmail.com>
2025-12-09 07:39:19 +0200

    sttt

--- api/wsgi.py
+++ api/wsgi.py
@@ -12,3 +12,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 # استدعاء Django application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(application)
