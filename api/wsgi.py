50ae305658e781f0ba0253e7668e3bfc3e6c762e
Mohamed Abed <momo55265526@gmail.com>
2025-12-23 07:31:13 +0200

    wfcds45

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from pathlib import Path
 
-# مسار مشروعك على PythonAnywhere
-project_home = '/home/mohamedelzafar/ecommerce'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+# إضافة مشروع Django إلى sys.path
+BASE_DIR = Path(__file__).resolve().parent.parent
+if str(BASE_DIR) not in sys.path:
+    sys.path.insert(0, str(BASE_DIR))
 
 # ضبط settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
@@ -12,7 +13,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 # استدعاء Django application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(application)
