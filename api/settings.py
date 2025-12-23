235d113143bd896de05435f92dfd4b954bae05ef
Mohamed Abed <momo55265526@gmail.com>
2025-12-23 19:02:26 +0200

    yjlk

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY= 'mdzl@idc&@j@3rmkj&0tvizeto^^5y0@3!wu1!mxysk=_r0o2l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False, cast=bool)
