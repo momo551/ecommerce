d231d918e4c435d81d6ae0b9b78c6f9f1c9a1803
Mohamed Abed <momo55265526@gmail.com>
2025-12-23 08:01:53 +0200

    fedc465

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
+# إضافة مسار المشروع
+project_home = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# ضبط settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
-application = get_wsgi_application()
+# استدعاء ASGI application
+from django.core.asgi import get_asgi_application
+application = get_asgi_application()
